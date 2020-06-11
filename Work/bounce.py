b923f997b2f56d1182647c9172762071c5613b54
rahul27raina <rahul27raina@gmail.com>
2020-06-11 22:42:45 +0530

    FirstCodePython

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+
+new_height = 100
+bounce_height = 3/5
+num_bounce = 1
+
+
+while num_bounce <= 10 :
+    new_height = round(new_height * bounce_height)
+    print ( num_bounce ,new_height)
+    num_bounce = num_bounce+1
+
+    
+
+
+
